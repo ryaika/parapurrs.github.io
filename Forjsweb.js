0a4bcfa39dcca23084fddfea09feba3c6dc032ac
ryaika <97860048+ryaika@users.noreply.github.com>
2022-04-21 02:01:05 +0800

    new files

---
+++ Forjsweb.js
@@ -0,0 +1,48 @@
+
+
+ $('#Food').mouseover(function() {
+    $('#Name1, #Desc1, #Other1, #btn1').css("visibility","visible");
+  });
+  
+  $('#Food').mouseout(function() {
+    $('#Name1, #Desc1, #Other1, #btn1').css("visibility","hidden");
+  });
+
+
+  $('#Toy').mouseover(function() {
+    $('#Name2, #Desc2, #Other2, #btn2').css("visibility","visible");
+  });
+  
+  $('#Toy').mouseout(function() {
+    $('#Name2, #Desc2, #Other2, #btn2').css("visibility","hidden");
+  });
+
+
+ 
+
+  $('#Clothes').mouseover(function() {
+    $('#Name3, #Desc3, #Other3, #btn3').css("visibility","visible");
+  });
+  
+  $('#Clothes').mouseout(function() {
+    $('#Name3, #Desc3, #Other3, #btn3').css("visibility","hidden");
+  });
+
+  $('#Suplements').mouseover(function() {
+    $('#Name5, #Desc5, #Other5, #btn5').css("visibility","visible");
+  });
+  
+  $('#Suplements').mouseout(function() {
+    $('#Name5, #Desc5, #Other5, #btn5').css("visibility","hidden");
+  });
+
+
+
+
+  $('#Game6').mouseover(function() {
+     $('#Name6, #Desc6, #Other6').css("visibility","visible");
+   });
+   
+   $('#Game6').mouseout(function() {
+     $('#Name6, #Desc6, #Other6').css("visibility","hidden");
+   });
